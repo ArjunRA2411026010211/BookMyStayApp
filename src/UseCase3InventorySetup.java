38e426f9e797fe1632ab90b562ab6441efafce99
Vishal242615 <vishalreddykamatham@gmail.com>
2026-03-13 15:25:49 +0530

    UC3: Centralized room inventory using HashMap

---
+++ src/UseCase3InventorySetup.java
@@ -0,0 +1,29 @@
+public class UseCase3InventorySetup {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hotel Room Inventory Status\n");
+
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        RoomInventory inventory = new RoomInventory();
+
+        System.out.println("Single Room:");
+        single.displayRoomDetails();
+        System.out.println("Available Rooms: " + inventory.getRoomAvailability().get("Single Room"));
+
+        System.out.println();
+
+        System.out.println("Double Room:");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + inventory.getRoomAvailability().get("Double Room"));
+
+        System.out.println();
+
+        System.out.println("Suite Room:");
+        suite.displayRoomDetails();
+        System.out.println("Available Rooms: " + inventory.getRoomAvailability().get("Suite Room"));
+    }
+}
