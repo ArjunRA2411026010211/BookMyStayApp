c7586c7232790c4229657a9a785a1b9ce4371038
Vishal242615 <vishalreddykamatham@gmail.com>
2026-03-13 15:35:26 +0530

    UC4: Implement room search and availability check

---
+++ src/UseCase4RoomSearch.java
@@ -0,0 +1,22 @@
+public class UseCase4RoomSearch {
+
+    public static void main(String[] args) {
+
+        System.out.println("Room Search\n");
+
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
+
+        RoomInventory inventory = new RoomInventory();
+
+        RoomSearchService searchService = new RoomSearchService();
+
+        searchService.searchAvailableRooms(
+                inventory,
+                singleRoom,
+                doubleRoom,
+                suiteRoom
+        );
+    }
+}
